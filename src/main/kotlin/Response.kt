6f56ba7661b458edd3438358e9a7ac221a3a27ac
luizfelipeairesoares <lfelipeas@gmail.com>
2020-05-29 11:41:25 -0300

    :art: Updates Response, adding two functions

--- src/main/kotlin/Response.kt
+++ src/main/kotlin/Response.kt
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.databind.JsonNode
 import spark.Response
 
 fun Response.withSuccess(): String {
@@ -5,6 +6,19 @@ fun Response.withSuccess(): String {
     this.header("content-type", "application/json")
 
     return "{\"success\" : ${true}}"
+}
+
+fun Response.withJsonString(jsonString: String): String {
+    val status = if (jsonString.isEmpty()) 204 else 200
+    this.status(status);
+    this.header("content-type", "application/json")
+    return jsonString
+}
+
+fun Response.withJson(json: JsonNode): String {
+    this.status(200)
+    this.header("content-type", "application/json")
+    return json.toPrettyString()
 }
 
 fun Response.withError(e: Exception): String {
