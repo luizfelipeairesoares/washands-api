f98cba44606f298b8b0325384cc5343846235302
luizfelipeairesoares <lfelipeas@gmail.com>
2020-05-29 11:39:31 -0300

    :sparkles: Creates new class, JsonTransformer

---
+++ src/main/kotlin/JsonTransformer.kt
@@ -0,0 +1,12 @@
+import com.google.gson.Gson
+import spark.ResponseTransformer
+
+class JsonTransformer : ResponseTransformer {
+
+    private val gson = Gson()
+
+    override fun render(model: Any): String {
+        return gson.toJson(model)
+    }
+
+}
